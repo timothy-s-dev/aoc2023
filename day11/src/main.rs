8f1c594188a0bfb5a24e97ef17b127d24250b3ff
Tim Sassone <tim.sassone@qgenda.com>
2023-12-05 11:33:49 -0500

    Add boilerplate for remaining puzzles

---
+++ day11/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let is_part_one = common::is_part_one();
+    let input_file_path = common::get_input_file_path();
+    println!("TODO: Complete puzzle (part {}) using file: {}",
+             if is_part_one { 1 } else { 2 },
+             input_file_path
+    );
+}
