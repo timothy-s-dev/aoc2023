3d284efb94370618523981befac9b42c7436eee6
timothy-s-dev <timothy.s.dev@gmail.com>
2023-12-17 16:53:14 -0500

    Started day 16

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,3 +1,43 @@
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Tile {
+    Empty, // .
+    VerticalSplitter, // |
+    HorizontalSplitter, // -
+    RightMirror, // /
+    LeftMirror, // \
+}
+impl Tile {
+    fn from_char(c: char) -> Tile {
+        match c {
+            '.' => Tile::Empty,
+            '|' => Tile::VerticalSplitter,
+            '-' => Tile::HorizontalSplitter,
+            '/' => Tile::RightMirror,
+            '\\' => Tile::LeftMirror,
+            _ => panic!("Unknown tile: {}", c),
+        }
+    }
+
+    fn get_new_directions(&self, direction: Direction) -> Vec<Direction> {
+        let mut new_directions: Vec<Direction> = Vec::new();
+
+        match *self {
+            Tile::Empty => new_directions.push(direction),
+            _ => todo!(),
+        };
+
+        new_directions
+    }
+}
+
 fn main() {
     let is_part_one = common::is_part_one();
     let input_file_path = common::get_input_file_path();
